824f98751d481646f5a9be27db84d919a4798772
JLchang8998 <moviedc496@gmail.com>
2023-06-07 01:52:54 +0800

    header已經混合儀錶板menu了，登入註冊還在做(但到時不會有header)

--- Dashboard/graph/urls.py
+++ Dashboard/graph/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include,re_path
+from graph import views_login
 from graph import views
 from graph import views_seller
 import graph
 
 urlpatterns = [
     path('seller_page/',views_seller.seller_page,name='seller_page'),
-    
+    path('login/',views_login.login_page,name='login_page'),
 ]
+
