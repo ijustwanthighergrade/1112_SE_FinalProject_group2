997377ab354249e20ec419340f369d20fa378dbb
10944143 <92848136+10944143@users.noreply.github.com>
2023-06-12 22:44:31 +0800

    chair

--- Dashboard/static/js/chair_page/chair1.js
+++ Dashboard/static/js/chair_page/chair1.js
@@ -3,9 +3,9 @@ function createChart() {
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: ["6/1", "6/2", "6/3", "6/4", "6/5"],
             datasets: [{
-                label: '# of Votes',
+                label: '使用次數',
                 data: [13, 19, 3, 5, 2, 3],
                 backgroundColor: [
                     'rgba(50, 99, 132, 0.2)',
@@ -15,13 +15,13 @@ function createChart() {
                 ],
                 borderWidth: 1
             },{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: '全店按摩椅使用次數',
+                data: [58, 80, 30, 50, 30, 40],
                 backgroundColor: [
-                    'rgba(2, 252, 2, 0.2)'
+                    'rgba(243, 177, 178, 0.5)'
                 ],
                 borderColor: [
-                    'rgba(2, 253, 2, 1)'
+                    'rgba(251, 219, 198, 1)'
                 ],
                 borderWidth: 1
             }]
--- Dashboard/static/js/chair_page/chair4.js
+++ Dashboard/static/js/chair_page/chair4.js
@@ -8,8 +8,12 @@ function createChart4() {
               label: 'My First Dataset',
               data: [305, 50],
               backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)'
+                'rgba(25, 99, 132, 0.2)',
+                'rgba(243, 177, 178, 0.5)'
+              ],
+              borderColor: [
+                'rgba(50, 99, 132, 1)',
+                'rgba(251, 219, 198, 1)'
               ],
               hoverOffset: 4
             }]
